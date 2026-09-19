5fe1620aa635b7dc442580d8300da6b79aaa1821
MohGaafer <mohamedahmedgaafer@gmail.com>
2026-09-19 15:02:34 +0300

    Add files via upload

--- js/products.js
+++ js/products.js
@@ -1063,7 +1063,7 @@ const customerReviews = [
 // SOCIAL MEDIA LINKS & CONTACT
 const socialLinks = {
   facebook: "https://www.facebook.com/tamtamvibes",
-  x: "https://x.com/tamara_vibes",
+  threads: "https://www.threads.com/@tamtama.vibes",
   pinterest: "https://www.pinterest.com/tamaravibes",
   instagram: "https://www.instagram.com/tamtama.vibes",
   youtube: "https://www.youtube.com/@Tamara-Vibes",
